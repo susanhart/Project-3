620effa284440953454cc5f516c9523e4f034f3b
Sue <susanhartesquire@yahoo.com>
2019-05-20 18:40:59 -0400

    Commit T-Shirt changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 $("#name").attr("focus", true);
+$("#job_role_other").hide();
+$("#select_theme").hide();
+$("#design").on("change", function() {
+   let value = $("#design").val()
+   console.log(value)
+   if (value === "js puns") {
+       $("option[value='tomato']").attr("selected", false);
+       $("option[value='cornflowerblue']").attr("selected", true);
+       $("option[value='cornflowerblue']").show(); 
+       $("option[value='darkslategrey']").show();
+       $("option[value='gold']").show();
+       $("option[value='tomato']").hide();
+       $("option[value='steelblue']").hide();
+       $("option[value='dimgrey']").hide();
+    }
+    else if(value==="heart js") {
+        $("option[value='tomato']").attr("selected", true);
+       $("option[value='cornflowerblue']").attr("selected", false);
+        $("option[value='tomato']").show(); 
+        $("option[value='steelblue']").show();
+        $("option[value='dimgrey']").show(); 
+        $("option[value='gold']").hide();
+        $("option[value='cornflowerblue']").hide();
+        $("option[value='darkslategrey']").hide(); 
+    }
+});
