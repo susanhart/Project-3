53a20c129e2dd7c1b3983a60607a65e1bcfb0963
Sue <susanhartesquire@yahoo.com>
2019-05-21 17:30:36 -0400

    Commit changes to Activities Section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let total=0
 $("#name").attr("focus", true);
 $("#job_role_other").hide();
 $("#select_theme").hide();
@@ -27,11 +28,73 @@ $("#design").on("change", function() {
 });
 $(".activities input").on("change", function() {
     let name = $(this).attr('name')
-    console.log(name)
+    // Get the value from a checked checkbox
+    let cost = $( "input[type=checkbox][name="+name+"]" ).attr("cost");
+    cost = parseInt(cost)
+    console.log(total)
     if (name==="js-frameworks") {
-        $("input[name='express']").attr("disabled", true);
+        if ($("input[name='express']").attr("disabled")) {
+            $("input[name='express']").attr("disabled", false); 
+            total-=cost  
+        }   else {
+            $("input[name='express']").attr("disabled", true);
+            total+=cost
+        }   
+    }
+    if (name==="express") {
+        if ($("input[name='js-frameworks']").attr("disabled")) {
+            $("input[name='js-frameworks']").attr("disabled", false); 
+            total-=cost  
+        }   else {
+            $("input[name='js-frameworks']").attr("disabled", true);
+            total+=cost
+        }   
     }
     if (name==="js-libs") {
-        $("input[name='node']").attr("disabled", true);
-    }    
+        if ($("input[name='node']").attr("disabled")) {
+            $("input[name='node']").attr("disabled", false); 
+            total-=cost  
+        }   else {
+            $("input[name='node']").attr("disabled", true);  
+            total+=cost
+        }
+    }  
+    if (name==="node") {
+        if ($("input[name='js-libs']").attr("disabled")) {
+            $("input[name='js-libs']").attr("disabled", false); 
+            total-=cost  
+        }   else {
+            $("input[name='js-libs']").attr("disabled", true);  
+            total+=cost
+        }
+    }  
+    if (name==="all") {  
+        
+        if ($("input[name='all']").is(':checked')) {
+            total+=cost     
+        }
+        else if ($("input[name='all']").is(':checked') == false) {
+            total-=cost    
+        }
+    }
+    
+    if (name==="build-tools") {  
+        
+        if ($("input[name='build-tools']").is(':checked')) {
+            total+=cost     
+        }
+        else  {
+            total-=cost    
+        }
+    }
+    if (name==="npm") {  
+        
+        if ($("input[name='npm']").is(':checked')) {
+            total+=cost     
+        }
+        else  {
+            total-=cost    
+        }
+    }
+    $("#total").text(total)
 });
